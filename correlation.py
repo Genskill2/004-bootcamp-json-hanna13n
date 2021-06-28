ec95fb87a03695647791605813189dc1812c3918
hanna13n <hanna.nechikkadan@gmail.com>
2021-06-28 18:20:36 +0530

    :D

--- correlation.py
+++ correlation.py
@@ -1,6 +1,7 @@
 # Add the functions in this file
 import json
 import math
+import sys
 
 
 def load_journal(journal):
@@ -19,22 +20,22 @@ def compute_phi(data, eve):
     n_1 = 0
     n_0 = 0
     for i in data:
-        if (i["squirrel"] and eve in i["events"]):
-            n11 += 1
+        if (i["squirrel"]):
             n1_ += 1
-            n_1 += 1
-        elif (not i["squirrel"] and eve not in i["events"]):
-            n00 += 1
+            if (eve in i["events"]):
+                n11 += 1
+                n_1 += 1
+            else:
+                n_0 += 1
+                n10 += 1
+        else:
             n0_ += 1
-            n_0 += 1
-        elif(i["squirrel"] and eve not in i["events"]):
-            n10 += 1
-            n1_ += 1
-            n_0 += 1
-        elif(not i["squirrel"] and eve in i["events"]):
-            n01 += 1
-            n0_ += 1
-            n_1 += 1
+            if eve in i["events"]:
+                n01 += 1
+                n_1 += 1
+            else:
+                n00 += 1
+                n_0 += 1
     phi = (n11*n00-n10*n01)/math.sqrt(n1_*n0_*n_1*n_0)
     return phi
 
@@ -51,15 +52,6 @@ def compute_correlations(journal):
 
 def diagnose(journal):
     corr = compute_correlations(journal)
-    max_eve = ""
-    max = -2
-    min = 2
-    min_eve = ""
-    for key, value in corr.items():
-        if(value > max):
-            max = value
-            max_eve = key
-        if(value < min):
-            min = value
-            min_eve = key
+    max_eve = max(corr, key=corr.get)
+    min_eve = max(corr, key=corr.get)
     return max_eve, min_eve
